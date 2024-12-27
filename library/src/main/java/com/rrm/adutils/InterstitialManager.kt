f1cf2662b31bd40c0d1a4c2e2b74c78e69f063f6
rodri <rodrigorm1091@gmail.com>
2024-12-26 17:44:27 -0800

    Suppressed warnings

--- library/src/main/java/com/rrm/adutils/InterstitialManager.kt
+++ library/src/main/java/com/rrm/adutils/InterstitialManager.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
+@Suppress("unused")
 class InterstitialManager {
     private var interstitial: InterstitialAd? = null
     private var interstitialId = "ca-app-pub-3940256099942544/1033173712" //AdMob test interstitial
